889f761f10b1c04811f98bbea6ddac1e2e6f2b31
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-24 11:54:11 +0100

    form layout created

--- Participants/src/mainPanel.java
+++ Participants/src/mainPanel.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class mainPanel {
+    private JPanel panelMain;
+    private JTextField fieldName;
+    private JFormattedTextField outFieldLog;
+    private JSpinner timeH;
+    private JButton addParticipantButton;
+    private JSpinner timeM;
+    private JFormattedTextField outFieldParticipants;
+
+
 }
