2f505bfb081fb6e17355e6ff02d5d9c163b6c8dd
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-24 12:10:37 +0100

    First working panel, thanking user for participating

--- Participants/src/mainPanel.java
+++ Participants/src/mainPanel.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class mainPanel {
+public class mainPanel extends JFrame {
     private JPanel panelMain;
     private JTextField fieldName;
     private JFormattedTextField outFieldLog;
@@ -10,4 +12,20 @@ public class mainPanel {
     private JFormattedTextField outFieldParticipants;
 
 
+    public mainPanel() {
+        addParticipantButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(addParticipantButton, "Hi, " + fieldName.getText() + "!\nThanks for participating!");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        mainPanel panel = new mainPanel();
+        panel.setContentPane(panel.panelMain);
+        panel.setSize(600, 400);
+        panel.setVisible(true);
+        panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
