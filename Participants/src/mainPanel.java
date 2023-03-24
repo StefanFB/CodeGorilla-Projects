f75297fbe69549e650dfb428e9cd65a676689769
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-03-24 16:14:59 +0100

    added check for valid name

--- Participants/src/mainPanel.java
+++ Participants/src/mainPanel.java
@@ -11,25 +11,26 @@ public class mainPanel extends JFrame {
     public JSpinner timeM;
     private JFormattedTextField outFieldParticipants;
 
-
-
-
     public mainPanel() {
         addParticipantButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String username = fieldName.getText();
+                int hours = (int) timeH.getValue();
+                int minutes = (int) timeM.getValue();
 
-
-                // Display success message to the user
-                JOptionPane.showMessageDialog(addParticipantButton, "Hi, " + username + "!\nThanks for participating!");
+                if (username.equals("")) { // Username has to be provided
+                    String msg = String.format("Please provide a valid name");
+                    JOptionPane.showMessageDialog(addParticipantButton, msg);
+                } else {  // Display success message to the user
+                    String msg = String.format("Thanks %s, you reserved at %02d:%02d", username, hours, minutes);
+                    JOptionPane.showMessageDialog(addParticipantButton, msg);
+                }
             }
         });
     }
 
-
     private void createUIComponents() {
-
         // Make sure the bounds of the spinner are set to the correct values for hours and minutes
         SpinnerNumberModel hours = new SpinnerNumberModel(0, 0, 23, 1);
         SpinnerNumberModel minutes = new SpinnerNumberModel(0,0,59,1);
